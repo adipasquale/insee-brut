92dca93d388151e317e74474501f69b3dbdf8fef
adrien di pasquale <adrien@dipasquale.fr>
2018-12-03 19:35:53 -0500

    fixing deploy of crawler with new mongo settings

--- scrapy-project/insee_crawler/pipelines.py
+++ scrapy-project/insee_crawler/pipelines.py
@@ -18,7 +18,7 @@ class MongoPipeline(object):
     def from_crawler(cls, crawler):
         return cls(
             mongo_uri=crawler.settings.get('MONGO_URI'),
-            mongo_db=crawler.settings.get('MONGO_DATABASE', 'insee_brut_items')
+            mongo_db=crawler.settings.get('MONGO_DATABASE')
         )
 
     def open_spider(self, spider):
--- scrapy-project/insee_crawler/settings.py
+++ scrapy-project/insee_crawler/settings.py
@@ -88,3 +88,6 @@ ITEM_PIPELINES = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+MONGO_URI=None
+MONGO_DATABASE="insee_brut"
