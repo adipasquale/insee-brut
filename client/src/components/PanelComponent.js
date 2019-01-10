a48f4776ec096cf4fa0cf9ca6ac01f17bf5cc6bc
Adrien Quillet <adrienquillet@gmail.com>
2019-01-10 23:23:20 +0100

    WIP first panel

--- client/src/components/PanelComponent.js
+++ client/src/components/PanelComponent.js
@@ -1,8 +1,21 @@
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 
-const PanelComponenent = greeting => (
+interface propTypes {
+  menu: Array;
+}
+const PanelComponenent = ({ menu }) => (
   <Fragment>
-    <div className="bg-moon-gray vh-100 fl w-10">{greeting}</div>
+    <div className="bg-moon-gray vh-100 fl w-10">
+      <ul>
+        {menu.map(element => (
+          <li>
+            <a href="#">{element.title}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+    {/* <PanelComponenent menu2={menu.link} /> */}
   </Fragment>
 );
 
--- client/src/containers/HomeContainer.js
+++ client/src/containers/HomeContainer.js
@@ -1,57 +1,104 @@
 import React, { Fragment } from 'react';
 import PanelComponent from '../components/PanelComponent';
 
-// const tree = {
-//   location: [
-//     {
-//       id: 0,
-//       title: 'New York',
-//       selected: false,
-//       key: 'location'
-//     },
-//     {
-//       id: 1,
-//       title: 'Dublin',
-//       selected: false,
-//       key: 'location'
-//     },
-//     {
-//       id: 2,
-//       title: 'California',
-//       selected: false,
-//       key: 'location'
-//     },
-//     {
-//       id: 3,
-//       title: 'Istanbul',
-//       selected: false,
-//       key: 'location'
-//     },
-//     {
-//       id: 4,
-//       title: 'Izmir',
-//       selected: false,
-//       key: 'location'
-//     },
-//     {
-//       id: 5,
-//       title: 'Oslo',
-//       selected: false,
-//       key: 'location'
-//     }
-//   ]
-// };
+const tree = [
+  {
+    id: 0,
+    title: '1.0.0',
+    selected: false,
+    key: 'location'
+  },
+  {
+    id: 1,
+    title: '2.0.0 *',
+    children: [
+      {
+        id: 1,
+        title: '2.1.0',
+        selected: false,
+        key: 'location'
+      },
+      {
+        id: 1,
+        title: '2.2.0 *',
+        selected: false,
+        key: 'location',
+        children: [
+          {
+            id: 1,
+            title: '2.2.1',
+            selected: false,
+            key: 'location'
+          },
+          {
+            id: 1,
+            title: '2.2.2',
+            selected: false,
+            key: 'location'
+          },
+          {
+            id: 1,
+            title: '2.2.3',
+            selected: false,
+            key: 'location'
+          },
+          {
+            id: 1,
+            title: '2.2.4',
+            selected: false,
+            key: 'location'
+          }
+        ]
+      },
+      {
+        id: 1,
+        title: '2.3.0',
+        selected: false,
+        key: 'location'
+      },
+      {
+        id: 1,
+        title: '2.4.0',
+        selected: false,
+        key: 'location'
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: '3.0.0',
+    selected: false,
+    key: 'location'
+  },
+  {
+    id: 3,
+    title: '4.0.0',
+    selected: false,
+    key: 'location'
+  },
+  {
+    id: 4,
+    title: '5.0.0',
+    selected: false,
+    key: 'location'
+  },
+  {
+    id: 5,
+    title: '6.0.0',
+    selected: false,
+    key: 'location'
+  }
+];
 class HomeContainer extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {};
   }
 
   render() {
-    const greeting = 'Bonjour!';
     return (
       <Fragment>
-        <PanelComponent greeting={greeting} />
+        <PanelComponent menu={tree} />
       </Fragment>
     );
   }
