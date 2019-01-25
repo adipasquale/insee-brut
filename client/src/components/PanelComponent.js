7e27b0b8e26c3cee4e85d3c51c9a112af7e5fcb3
Adrien Quillet <adrienquillet@gmail.com>
2019-01-25 23:01:49 +0100

    WIP menu again

--- client/src/components/PanelComponent.js
+++ client/src/components/PanelComponent.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const PanelComponent = () => {
-  return <div bg-dark-red>coucou</div>;
+const PanelComponent = ({ menu }) => {
+  return (
+    <div className="bg-moon-gray vh-100 fl w-10">
+      <ul>
+        {menu.map(el => {
+          return <li>{el.id}</li>;
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default PanelComponent;
--- client/src/containers/HomeContainer.js
+++ client/src/containers/HomeContainer.js
@@ -5,7 +5,6 @@ const tree = [
   {
     id: 0,
     title: "1.0.0",
-    selected: false,
     key: "location"
   },
   {
@@ -15,37 +14,31 @@ const tree = [
       {
         id: 11,
         title: "2.1.0",
-        selected: false,
         key: "location"
       },
       {
         id: 12,
         title: "2.2.0 *",
-        selected: false,
         key: "location",
         children: [
           {
             id: 121,
             title: "2.2.1",
-            selected: false,
             key: "location"
           },
           {
             id: 122,
             title: "2.2.2",
-            selected: false,
             key: "location"
           },
           {
             id: 123,
             title: "2.2.3",
-            selected: false,
             key: "location"
           },
           {
             id: 124,
             title: "2.2.4",
-            selected: false,
             key: "location"
           }
         ]
@@ -53,13 +46,11 @@ const tree = [
       {
         id: 13,
         title: "2.3.0",
-        selected: false,
         key: "location"
       },
       {
         id: 14,
         title: "2.4.0",
-        selected: false,
         key: "location"
       }
     ]
@@ -67,33 +58,32 @@ const tree = [
   {
     id: 2,
     title: "3.0.0",
-    selected: false,
     key: "location"
   },
   {
     id: 3,
     title: "4.0.0",
-    selected: false,
     key: "location"
   },
   {
     id: 4,
     title: "5.0.0",
-    selected: false,
     key: "location"
   },
   {
     id: 5,
     title: "6.0.0",
-    selected: false,
     key: "location"
   }
 ];
 class HomeContainer extends React.Component {
+  constructor(props) {
+    super(props), (this.state = {});
+  }
   render() {
     return (
       <Fragment>
-        <PanelComponent />
+        <PanelComponent menu={tree} />
       </Fragment>
     );
   }
